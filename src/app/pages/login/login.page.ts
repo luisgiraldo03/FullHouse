8211847e5e8b5d293f447231e82fc534574b9d4f
josewweee <josemb125@gmail.com>
2020-05-26 22:11:43 -0500

    inyectando servicio de login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,50 +3,60 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 import { NavController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 
+//SERVICIO DE AUTH
+import { AuthenticationService } from '../../services/authentication.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
+  errorMessage: String = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private navCtrl: NavController,
+    private storage: Storage,
+    private authService: AuthenticationService
+  ) {}
+
+  ngOnInit() {
+    // EL FORM SE USARA PARA VALIDAR CON COLORES MIENTRAS EL USUARIO ESCRIBE
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(
+        '',
+        Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')])
+      ),
+      password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(4)]))
+    });
+  }
 
   // MENSAJES DE ERROR EN CASO DE QUE SE PONGA MAL LOS DATOS
   validationMessage = {
     email: [
       { type: 'required', message: 'El email es requerido' },
-      { type: 'pattern', message: 'Este no es un email valido' },
+      { type: 'pattern', message: 'Este no es un email valido' }
     ],
     password: [
       { type: 'required', message: 'La contraseña es requerida' },
-      { type: 'min', message: 'Minimo 4 caracteres para la contraseña' },
-    ],
+      { type: 'min', message: 'Minimo 4 caracteres para la contraseña' }
+    ]
   };
-  errorMessage: String = '';
-
-  constructor(private formBuilder: FormBuilder, private navCtrl: NavController, private storage: Storage) {
-    // EL FORM SE USARA PARA VALIDAR CON COLORES MIENTRAS EL USUARIO ESCRIBE
-    this.loginForm = this.formBuilder.group({
-      email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
-      password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(4)])),
-    });
-  }
-
-  ngOnInit() {}
 
   // inventamos un servicio que hace las de backend
   loginUser(credentials) {
-    // this.authService.loginUser(credentials).then(res => {
-    //   this.errorMessage = '';
-    //   this.storage.set('isUserLoggedIn', true);
-    //   // navegacion diferente
-    //   this.navCtrl.navigateForward('/menu/home');
-    // })
-    // .catch(err => {
-    //   this.errorMessage = err;
-    // });
-    this.storage.set('isUserLogged', true);
-    this.navCtrl.navigateForward('/home');
+    this.authService.loginUser(credentials).then(
+      (res) => {
+        this.errorMessage = '';
+        this.storage.set('isUserLogged', true);
+        this.navCtrl.navigateForward('/home');
+      },
+      (err) => {
+        this.errorMessage = err.message;
+      }
+    );
   }
 
   goToRegister() {
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
 
+//BASE DE DATOS
+import { AngularFireAuth } from '@angular/fire/auth';
+
+//COMO ESTA PROVIDED IN ROOT, NO NECESITAMOS INYECTARLO EN APP.MODULES
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  constructor(private auth: AngularFireAuth) {}
 
-  constructor() { }
+  registerUser(credentials) {
+    return new Promise<any>((resolve, reject) => {
+      this.auth.createUserWithEmailAndPassword(credentials.email, credentials.password).then(
+        (res) => resolve(res),
+        (err) => reject(err)
+      );
+    });
+  }
+
+  loginUser(credentials) {
+    return new Promise<any>((resolve, reject) => {
+      this.auth.signInWithEmailAndPassword(credentials.email, credentials.password).then(
+        (res) => resolve(res),
+        (err) => reject(err)
+      );
+    });
+  }
 }
